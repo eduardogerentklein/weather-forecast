8d5dd9678bdbd750fc9d50b26df2c0bf1a449ae7
Eduardo Klein <eduardo.klein@catolicasc.org.br>
2021-10-02 15:41:29 -0300

    Update greeting message

--- components/Greetings.js
+++ components/Greetings.js
@@ -2,10 +2,19 @@ import H5 from './H5'
 import Paragraph from './Paragraph'
 
 const Greetings = ({ userName, showUserName }) => {
+  const currentDate = new Date()
+  const hours = currentDate.getHours()
+  const greeting =
+    hours < 12 || hours < 18 ? 'Have a nice day' : 'Have a nice evening'
+  const greetingMessage =
+    hours < 12 ? 'Good Morning' : hours < 18 ? 'Good Afternoon' : 'Good Evening'
   return (
     <header className='p-10'>
-      <H5>Good Afternoon{showUserName && `, ${userName}`}</H5>
-      <Paragraph className='text-white-75'>Have a nice day</Paragraph>
+      <H5>
+        {greetingMessage}
+        {showUserName && `, ${userName}`}
+      </H5>
+      <Paragraph className='text-white-75'>{greeting}</Paragraph>
     </header>
   )
 }
